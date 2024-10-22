af095355d92b53ed38d497ac258018f97c25ef9c
fdaniel27 <141162960+fdaniel27@users.noreply.github.com>
2024-10-22 16:38:04 -0300

    Importei bibliotecas e implementei uma função para salvar dados

--- treinos.py
+++ treinos.py
@@ -1,1 +1,11 @@
+import csv
+import random
+from datetime import datetime
+
+def salvar_dados(registros):
+    with open('treinos_competicoes.csv', mode='w', newline='') as arquivo:
+        escritor = csv.writer(arquivo)
+        escritor.writerow(['Data', 'Tipo', 'Distância', 'Tempo', 'Localização', 'Condições'])
+        for registro in registros:
+            escritor.writerow(registro)
 
